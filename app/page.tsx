777dcaa3fbfa199c424386f945cc33f2d082fd31
myk <michaelmugendi1707@gmail.com>
2024-08-27 10:04:20 +0300

    Update: first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,62 @@
+"use client";
 import Image from "next/image";
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from "powerbi-client";
+import React from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <PowerBIEmbed
+        embedConfig={{
+          type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+          id: "d90d3b4d-dd3e-46db-90cc-0cf63d7935c8",
+          embedUrl:
+            "https://app.powerbi.com/reportEmbed?reportId=d90d3b4d-dd3e-46db-90cc-0cf63d7935c8&groupId=18116d7c-94ff-46ce-8732-0f023fe9d2d0&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUVBU1QyLUItUFJJTUFSWS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",
+          accessToken:
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktRMnRBY3JFN2xCYVZWR0JtYzVGb2JnZEpvNCIsImtpZCI6IktRMnRBY3JFN2xCYVZWR0JtYzVGb2JnZEpvNCJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNzQ3YjEyNTEtNWRmNC00MzMwLTllMTktYzNjZjI4YjMxODUyLyIsImlhdCI6MTcyNDY1NDQ2MywibmJmIjoxNzI0NjU0NDYzLCJleHAiOjE3MjQ2NTgzNjksImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84WEFBQUE3ZFFJNjV2ZjBPN2piQlFvb3lIeUZrSmJoSnF2NlB2Sy9OTHppc0VPVjY5Skk3NnZQZlZZd1FhWnhra1dBRFlwck9icGN3VHZZZ2pZTzlFNzZJZW1FS01XUEtkWjM4YVBWRkdNN0JrSEVkQT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJBbmFseXRpY3MiLCJnaXZlbl9uYW1lIjoiTXVuaWJpbGxpbmciLCJpZHR5cCI6InVzZXIiLCJpcGFkZHIiOiI0MS41OS4xNTQuMTM2IiwibmFtZSI6Ik11bmliaWxsaW5nIEFuYWx5dGljcyIsIm9pZCI6IjdkZGM4NmQzLTRhZGMtNGEzNS1iNWQ5LTllYWY5YTU1MjUyMiIsInB1aWQiOiIxMDAzMjAwMzFEODZBQkQ1IiwicmgiOiIwLkFYMEFVUko3ZFBSZE1FT2VHY1BQS0xNWVVna0FBQUFBQUFBQXdBQUFBQUFBQUFDMUFJOC4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJnS2pvVHJ0RXBhaktyNmYtTUJmZEFWTkNlLXp0N196cWo0RWpSYlp1NkY4IiwidGlkIjoiNzQ3YjEyNTEtNWRmNC00MzMwLTllMTktYzNjZjI4YjMxODUyIiwidW5pcXVlX25hbWUiOiJtdW5pYmlsbGluZy5hbmFseXRpY3NAY3Jlc3RsaW5lc29mdHdhcmUub25taWNyb3NvZnQuY29tIiwidXBuIjoibXVuaWJpbGxpbmcuYW5hbHl0aWNzQGNyZXN0bGluZXNvZnR3YXJlLm9ubWljcm9zb2Z0LmNvbSIsInV0aSI6Im4zNU5aSXhDT0UyVEMzalBSZ1ZmQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImYyZWY5OTJjLTNhZmItNDZiOS1iN2NmLWExMjZlZTc0YzQ1MSIsImE5ZWE4OTk2LTEyMmYtNGM3NC05NTIwLThlZGNkMTkyODI2YyIsIjliODk1ZDkyLTJjZDMtNDRjNy05ZDAyLWE2YWMyZDVlYTVjMyIsImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfaWRyZWwiOiIxIDE2IiwieG1zX3BsIjoiZW4tVVMifQ.F56i4PcLJq2nqgKr3VfYAhnNFbOQfPZOJv2gF_gR05uV5n82Jxu4gpSmcpXKNRK1o4NxWGuicqB0unAA88aP-7W56n8dxoIQ2HjSPSmDQNq0gSs011rjevRLQmWRiN-EYvKcv3U5AAfzrps4_wTkPhQOTDHd1M-QL_iidzIcJ4ZJuRWq9jb7B4KK4QP3u_O39S5kXF5kFXp5S9vddasp04aY1Q-El6sNZpuOcLwqIpHWpkZw3fNR8mubLSKj51R3TpC50VOWvTJCL5gE7AIgZ7KAlOhWZBZpRch_Bz8jhsNxS-Z3QufWv7htvGX15g-NZ5X7AJVkeC7xNLWSNy3f8w",
+          tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
+              },
+            },
+            background: models.BackgroundType.Transparent,
+          },
+        }}
+        eventHandlers={
+          new Map([
+            [
+              "loaded",
+              function () {
+                console.log("Report loaded");
+              },
+            ],
+            [
+              "rendered",
+              function () {
+                console.log("Report rendered");
+              },
+            ],
+            [
+              "error",
+              function (event: any) {
+                console.log(event.detail);
+              },
+            ],
+            ["visualClicked", () => console.log("visual clicked")],
+            ["pageChanged", (event) => console.log(event)],
+          ])
+        }
+        cssClassName={"Embed-container"}
+        getEmbeddedComponent={(embeddedReport) => {
+          //@ts-ignore
+          window.report = embeddedReport as Report;
+        }}
+      />
     </main>
   );
 }
